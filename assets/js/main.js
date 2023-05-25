c764af6eb8e74eaf2137e1e0751ef0096b878cac
kitkat0202 <he.katherine321@gmail.com>
2023-05-25 00:55:27 -0400

    FEAT: add video and video modals

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
       var modalID = el.getAttribute("data-modal-id");
       var modal = document.querySelector("#".concat(modalID));
       modal.classList.add("active");
+      document.querySelector("body").classList.add("pause");
     });
   }); /////////////////
   // CLOSE MODAL //
@@ -18,8 +19,15 @@ document.addEventListener('DOMContentLoaded', function () {
   var allModals = document.querySelectorAll(".modal");
 
   function closeModal() {
+    document.querySelector("body").classList.remove("pause");
     allModals.forEach(function (el) {
       el.classList.remove("active");
+      var elVideo = el.querySelector("video");
+
+      if (elVideo) {
+        elVideo.pause();
+        elVideo.currentTime = 0;
+      }
     });
   } /////////////////
   // INTRO MODAL //
@@ -27,9 +35,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
   var introModal = document.querySelector("#intro-modal");
-  introModal.addEventListener("click", function () {
-    document.querySelector("body").classList.remove("pause");
-    closeModal();
+  introModal.addEventListener("click", closeModal);
+  var allVideoModal = document.querySelectorAll(".video-modal");
+  allVideoModal.forEach(function (el) {
+    el.addEventListener("click", closeModal);
   }); ///////////////////
   // Mobile Navbar //
   ///////////////////
@@ -74,34 +83,28 @@ document.addEventListener('DOMContentLoaded', function () {
   } ////////////
   // SLIDES //
   ////////////
+  // var index = 0;
+  // var slides = document.querySelectorAll(".slides");
+  // var dot = document.querySelectorAll(".dot");
+  // function changeSlide(){
+  //   if(index<0){
+  //     index = slides.length-1;
+  //   }
+  //   if(index>slides.length-1){
+  //     index = 0;
+  //   }
+  //   for(let i=0;i<slides.length;i++){
+  //     slides[i].classList.remove("active");
+  //     dot[i].classList.remove("active");
+  //   }
+  //   let nextActiveSlide = document.querySelector(`[data-slide-num="${index}"]`)
+  //   let nextActiveDot = document.querySelector(`[data-dot-num="${index}"]`)
+  //   nextActiveSlide.classList.add("active");
+  //   nextActiveDot.classList.add("active");
+  //   index++;
+  //   setTimeout(changeSlide,5000);
+  // }
+  // changeSlide();
 
-
-  var index = 0;
-  var slides = document.querySelectorAll(".slides");
-  var dot = document.querySelectorAll(".dot");
-
-  function changeSlide() {
-    if (index < 0) {
-      index = slides.length - 1;
-    }
-
-    if (index > slides.length - 1) {
-      index = 0;
-    }
-
-    for (var i = 0; i < slides.length; i++) {
-      slides[i].classList.remove("active");
-      dot[i].classList.remove("active");
-    }
-
-    var nextActiveSlide = document.querySelector("[data-slide-num=\"".concat(index, "\"]"));
-    var nextActiveDot = document.querySelector("[data-dot-num=\"".concat(index, "\"]"));
-    nextActiveSlide.classList.add("active");
-    nextActiveDot.classList.add("active");
-    index++;
-    setTimeout(changeSlide, 5000);
-  }
-
-  changeSlide();
 });
 //# sourceMappingURL=main.js.map
